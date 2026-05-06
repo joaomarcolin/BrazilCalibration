a8616bbb92153a9f80f16a43cb30a92ea00aaf0b
joaomarcolin <jvmarcolin@hotmail.com>
2026-05-06 13:31:04 +0200

    I checked that report1.Rmd works very well until the LULC section, but didn't check the next sections (labor intensity, land concentration, area by activity). I also did some minor changes to main.R.

--- main.R
+++ main.R
@@ -28,9 +28,19 @@ rmarkdown::render("data_sources.Rmd", output_file = "data_sources.html", quiet=T
 
 # Settings for generating the cell grid -----------------------------------
 # The settings bellow are only used from section (3) on
+#
+# I ran everything with set_subset==FALSE and param_plot_km set to 1 or 5, so 
+# we have the full country at the 1km or 5km resolution. Since the spatial processing
+# takes a long time to complete, I thought it was a good idea to make the code more flexible so that
+# we can process a small part of the country. This may be useful if we ever need a higher resolution (<1km).
+# In that case, you should pick: a biome, OR a set of states, OR a biome and a set of states - in this last case,
+# the code creates the grid for the largest contiguous area of the overlap between the selected biome and the selected states.
+#
+# When working with the 1km or 5km resolution, it's best to just use the grid that is already created.
+# If you need to look into a specific region, just use dplyr::filter() and select by municipality code, 
+# state acronym or any other condition you see fit.
 
 param_plot_km <- 5        # must be a positive integer
-#param_year    <- 1985     # must be 1985, 1995, 2006 or 2017
 set_subset    <- FALSE    # if TRUE, the cell grid is constructed only for the set of states or the biome specified below
 set_name      <- "brazil" # name the area of interest, used to save the final cell grid
 
@@ -44,31 +54,31 @@ if (set_subset) {
   set_subset_area <- c("GO", "MT", "MS", "MA", "TO", "PI", "BA", "DF")
 }
 
-## (1) cell grid -----------------------------------------------------------
-## creates output folders
-#if (!dir.exists("data_outputs/1_cell_grid/1_reproject"))     dir.create("data_outputs/1_cell_grid/1_reproject")
-#if (!dir.exists("data_outputs/1_cell_grid/2_recode_lulc"))   dir.create("data_outputs/1_cell_grid/2_recode_lulc")
-#if (!dir.exists("data_outputs/1_cell_grid/3_create_grid"))   dir.create("data_outputs/1_cell_grid/3_create_grid")
-#if (!dir.exists("data_outputs/1_cell_grid/4_treat_grid"))    dir.create("data_outputs/1_cell_grid/4_treat_grid")
-#if (!dir.exists("data_outputs/1_cell_grid/5_grid_lulc"))     dir.create("data_outputs/1_cell_grid/5_grid_lulc")
-#if (!dir.exists("data_outputs/1_cell_grid/6_complete_grid")) dir.create("data_outputs/1_cell_grid/6_complete_grid")
-### create grid and calculate plot-level values
-##source("scripts/spatial1_reproject.R")     # reproject shapefiles and rasters # only need to run it once
-##source("scripts/spatial2_recode_lulc.R")   # reclass Mapbiomas' rasters       # only need to run it once
-#source("scripts/spatial3_create_grid.R")   # create grid
-#source("scripts/spatial4_treat_grid.R")    # identify farms
-#source("scripts/spatial5_grid_lulc.R")     # calculate plot-level LULC variables
-#source("scripts/spatial6_complete_grid.R") # generate final grid
-#
-## save final data for calibration
-#readr::write_csv(df_grid, paste0("data_outputs/1_cell_grid/6_complete_grid/df_",set_name,"_grid_",param_plot_km,"km.csv"))
-#sf::st_write(sf_grid,     paste0("data_outputs/1_cell_grid/6_complete_grid/sf_",set_name,"_grid_",param_plot_km,"km.shp"), delete_layer=TRUE)
+# (1) cell grid -----------------------------------------------------------
+# creates output folders
+if (!dir.exists("data_outputs/1_cell_grid/1_reproject"))     dir.create("data_outputs/1_cell_grid/1_reproject")
+if (!dir.exists("data_outputs/1_cell_grid/2_recode_lulc"))   dir.create("data_outputs/1_cell_grid/2_recode_lulc")
+if (!dir.exists("data_outputs/1_cell_grid/3_create_grid"))   dir.create("data_outputs/1_cell_grid/3_create_grid")
+if (!dir.exists("data_outputs/1_cell_grid/4_treat_grid"))    dir.create("data_outputs/1_cell_grid/4_treat_grid")
+if (!dir.exists("data_outputs/1_cell_grid/5_grid_lulc"))     dir.create("data_outputs/1_cell_grid/5_grid_lulc")
+if (!dir.exists("data_outputs/1_cell_grid/6_complete_grid")) dir.create("data_outputs/1_cell_grid/6_complete_grid")
+## create grid and calculate plot-level values
+#source("scripts/spatial1_reproject.R")     # reproject shapefiles and rasters # only need to run it once
+#source("scripts/spatial2_recode_lulc.R")   # reclass Mapbiomas' rasters       # only need to run it once
+source("scripts/spatial3_create_grid.R")   # create grid
+source("scripts/spatial4_treat_grid.R")    # identify farms
+source("scripts/spatial5_grid_lulc.R")     # calculate plot-level LULC variables
+source("scripts/spatial6_complete_grid.R") # generate final grid
+
+# save final data for calibration
+readr::write_csv(df_grid, paste0("data_outputs/1_cell_grid/6_complete_grid/df_",set_name,"_grid_",param_plot_km,"km.csv"))
+sf::st_write(sf_grid,     paste0("data_outputs/1_cell_grid/6_complete_grid/sf_",set_name,"_grid_",param_plot_km,"km.shp"), delete_layer=TRUE)
 
 # (2) group municipalities ------------------------------------------------
 # most data is available at the municipality-level, and many municipalities were
 # created after 1985, so there are many NAs. I group municipalities into a few groups
 # so data can be aggregated at the municipality-group-level consistently through 1985-2024
-source("scripts/group_municipalities.R")
+source("scripts/group_municipalities.R") # takes about 5 min to run
 readr::write_csv(df_brazil_mun, "data_outputs/2_brazil_mun/df_brazil_mun.csv")
 
 # (3) agriculture census data ---------------------------------------------
@@ -93,11 +103,6 @@ readr::write_csv(df_yearly_mun,    "data_outputs/4_yearly_data/df_yearly_mun.csv
 readr::write_csv(df_yearly_prices, "data_outputs/4_yearly_data/df_prices.csv")
 
 # (5) report results ------------------------------------------------------
-#source("scripts/census3_get_parameters.R") # calculate model parameters
-#source("scripts/yearly3_sort_amc.R")          # sort municipalities by AMC
-#source("scripts/yearly4_calculate_yield.R")   # calculate yield
-#source("scripts/yearly5_price_series.R")      # get yearly country-level variables
-
 # define which states will be included in the report
 # "SP" and "MG" also have significant Cerrado area but were not included
 report_states <- c("GO", "MT", "MS", "MA", "TO", "PI", "BA", "DF")
@@ -107,6 +112,80 @@ rmarkdown::render("report1.Rmd", output_file = "report1.html")
 #rmarkdown::render("report2.Rmd", output_file = "report2.html")
 
 # (6) final results -------------------------------------------------------
+
+## load inputs if needed
+## (uncomment this to avoid running previous scripts)
+#df_brazil_mun    <- readr::read_csv(
+#                      "data_outputs/2_brazil_mun/df_brazil_mun.csv",
+#                      col_types = cols(
+#                        .default               = col_character(),
+#                        legal_amazon           = col_logical(),
+#                        amazon_area_km2        = col_number(),
+#                        atlantic_area_km2      = col_number(),
+#                        caatinga_area_km2      = col_number(),
+#                        cerrado_area_km2       = col_number(),
+#                        islands_area_km2       = col_number(),
+#                        pampa_area_km2         = col_number(),
+#                        pantanal_area_km2      = col_number(),
+#                        mun_area_km2           = col_number(),
+#                        mun_protected_area_km2 = col_number()
+#                        )
+#                      )
+#
+#df_census_mun    <- readr::read_csv(
+#                      "data_outputs/3_census_data/df_census_mun.csv",
+#                      col_types = cols(
+#                        .default      = col_character(),
+#                        farm_area_ha  = col_number(),
+#                        n_farms       = col_number(),
+#                        activity_area = col_number(),
+#                        workers       = col_number()
+#                        )
+#                      )
+#
+#df_census_state  <- readr::read_csv(
+#                      "data_outputs/3_census_data/df_census_state.csv",
+#                      col_types = cols(
+#                        .default      = col_character(),
+#                        farm_area_ha  = col_number(),
+#                        n_farms       = col_number(),
+#                        activity_area = col_number(),
+#                        workers       = col_number()
+#                        )
+#                      )
+#
+#df_yearly_mun    <- readr::read_csv(
+#                      "data_outputs/4_yearly_data/df_yearly_mun.csv",
+#                      col_types = cols(
+#                        .default       = col_character(),
+#                        pam_area_ha    = col_number(),
+#                        pam_output_ton = col_number(),
+#                        ppm_herd       = col_number(),
+#                        mb_natcover_ha = col_number(),
+#                        mb_nonveg_ha   = col_number(),
+#                        mb_notobs_ha   = col_number(),
+#                        mb_pasture_ha  = col_number(),
+#                        mb_soybean_ha  = col_number(),
+#                        mb_tcrop_ha    = col_number(),
+#                        mb_forestry_ha = col_number(),
+#                        mb_pcrop_ha    = col_number()
+#                        )
+#                      )
+#
+#df_yearly_prices <- readr::read_csv(
+#                      "data_outputs/4_yearly_data/df_prices.csv",
+#                      col_types = cols(
+#                        .default = col_number(),
+#                        year     = col_character()
+#                        )
+#                      )
+
+# PREVIOUS (OUTDATED) SCRIPTS
+#source("scripts/census3_get_parameters.R") # calculate model parameters
+#source("scripts/yearly3_sort_amc.R")          # sort municipalities by AMC
+#source("scripts/yearly4_calculate_yield.R")   # calculate yield
+#source("scripts/yearly5_price_series.R")      # get yearly country-level variables
+
 source("scripts/final1_data.R")           # group municipality-level data
 source("scripts/final2_initialization.R") # generate final grid_df
 
